56f0d7dca5ab27c5bab29db711f0d8f456361db0
yuyang <yuyangjerry1@gmail.com>
2023-04-27 15:23:19 +1000

    Initial Commit

---
+++ FlexNet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlexNet
+//
+//  Created by Yuyang Ji on 27/4/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
